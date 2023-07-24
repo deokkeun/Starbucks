b6c713714ed3aad823f6f48a4adcc4074ca8ef55
deokkeun <deokk91@gmail.com>
2023-07-24 12:25:01 +0900

    Feat: youtube iframe API

--- js/main.js
+++ js/main.js
@@ -69,8 +69,49 @@ new Swiper('.promotion .swiper', {
     el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
     clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
   },
-  navigaion: {
+  navigation: {
     prevEl: '.promotion .swiper-prev',
     nextEl: '.promotion .swiper-next'
   }
 });
+
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function() {
+  isHidePromotion = !isHidePromotion;
+  if(isHidePromotion) {
+    // 숨김 처리!
+    promotionEl.classList.add('hide');
+  } else {
+    // 보임 처리!
+    promotionEl.classList.remove('hide');
+  }
+});
+
+
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    { // 옵션
+      y: size,
+      repeat: -1,
+      yoyo: true,
+      ease: Power1.easeInOut,
+      delay: random(0, delay)
+  });
+}
+floatingObject('.floating', 1, 15);
+floatingObject('.floating', .5, 15);
+floatingObject('.floating', 1.5, 20);
